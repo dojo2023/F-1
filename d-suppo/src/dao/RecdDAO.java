a912a8edd6a7813a36228bdd1fccbb8ddf830c70
dojo2023-hirai <130331130+dojo2023-hirai@users.noreply.github.com>
2023-06-20 11:01:08 +0900

    RECD 6/20修正

--- d-suppo/src/dao/RecdDAO.java
+++ d-suppo/src/dao/RecdDAO.java
@@ -13,158 +13,153 @@ import model.Recd;
 public class RecdDAO {
 	// 引数paramで検索項目を指定し、検索結果のリストを返す
 
-		public List<Recd> selectGenre(int genre) {
-			Connection conn = null;
-			List<Recd> genreList = new ArrayList<Recd>();
+	public List<Recd> selectGenre(int genre) {
+		Connection conn = null;
+		List<Recd> genreList = new ArrayList<Recd>();
 
-			try {
-				// JDBCドライバを読み込む
-				Class.forName("org.h2.Driver");
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
 
-				// データベースに接続する
-				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/test", "sa", "");
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/test", "sa", "");
 
-				// SQL文を準備する
-				String sql = "SELECT DIETNAME,DIETPIC FROM RECD WHERE GENRE = ? ORDER BY RAND() LIMIT 3";
-				PreparedStatement pStmt = conn.prepareStatement(sql);
+			// SQL文を準備する
+			String sql = "SELECT DIETNAME,DIETPIC FROM RECD WHERE GENRE = ? ORDER BY RAND() LIMIT 3";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-				// SQL文を完成させる
-				pStmt.setInt(1, genre);
+			// SQL文を完成させる
+			pStmt.setInt(1, genre);
 
-				// SQL文を実行し、結果表を取得する
-				ResultSet rs = pStmt.executeQuery();
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
 
-				// 結果表をコレクションにコピーする
-				while (rs.next()) {
-					Recd genreResult = new Recd(
-							rs.getString("DIETNAME"),
-							rs.getString("DIETPIC")
-							);
-					genreList.add(genreResult);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				genreList = null;
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				genreList = null;
-			} finally {
-				// データベースを切断
-				if (conn != null) {
-					try {
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-						genreList = null;
-					}
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Recd genreResult = new Recd(
+						rs.getString("DIETNAME"),
+						rs.getString("DIETPIC"));
+				genreList.add(genreResult);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			genreList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			genreList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					genreList = null;
 				}
 			}
-
-			// 結果を返す
-			return genreList;
 		}
 
-
-		public List<Recd> selectDish(int dish) {
-			Connection conn = null;
-			List<Recd> genreList = new ArrayList<Recd>();
-
-			try {
-				// JDBCドライバを読み込む
-				Class.forName("org.h2.Driver");
-
-				// データベースに接続する
-				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/test", "sa", "");
-
-				// SQL文を準備する
-				String sql = "SELECT DIETNAME,DIETPIC FROM RECD WHERE Dish = ? ORDER BY RAND() LIMIT 3";
-				PreparedStatement pStmt = conn.prepareStatement(sql);
-
-				// SQL文を完成させる
-				pStmt.setInt(1, dish);
-
-				// SQL文を実行し、結果表を取得する
-				ResultSet rs = pStmt.executeQuery();
-
-				// 結果表をコレクションにコピーする
-				while (rs.next()) {
-					Recd genreResult = new Recd(
-							rs.getString("DIETNAME"),
-							rs.getString("DIETPIC")
-							);
-					genreList.add(genreResult);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				genreList = null;
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				genreList = null;
-			} finally {
-				// データベースを切断
-				if (conn != null) {
-					try {
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-						genreList = null;
-					}
-				}
-			}
-
-			// 結果を返す
-			return genreList;
-		}
-
-
-		public List<Recd> selectHarvest(int harvest) {
-			Connection conn = null;
-			List<Recd> genreList = new ArrayList<Recd>();
-
-			try {
-				// JDBCドライバを読み込む
-				Class.forName("org.h2.Driver");
-
-				// データベースに接続する
-				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/test", "sa", "");
-
-				// SQL文を準備する
-				String sql = "SELECT DIETNAME,DIETPIC FROM RECD WHERE GENRE = ? ORDER BY RAND() LIMIT 3";
-				PreparedStatement pStmt = conn.prepareStatement(sql);
-
-				// SQL文を完成させる
-				pStmt.setInt(1, harvest);
-
-				// SQL文を実行し、結果表を取得する
-				ResultSet rs = pStmt.executeQuery();
-
-				// 結果表をコレクションにコピーする
-				while (rs.next()) {
-					Recd genreResult = new Recd(
-							rs.getString("DIETNAME"),
-							rs.getString("DIETPIC")
-							);
-					genreList.add(genreResult);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				genreList = null;
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				genreList = null;
-			} finally {
-				// データベースを切断
-				if (conn != null) {
-					try {
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-						genreList = null;
-					}
-				}
-			}
-
-			// 結果を返す
-			return genreList;
-		}
+		// 結果を返す
+		return genreList;
 	}
+
+	public List<Recd> selectDish(int dish) {
+		Connection conn = null;
+		List<Recd> genreList = new ArrayList<Recd>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/test", "sa", "");
+
+			// SQL文を準備する
+			String sql = "SELECT DIETNAME,DIETPIC FROM RECD WHERE Dish = ? ORDER BY RAND() LIMIT 3";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, dish);
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Recd genreResult = new Recd(
+						rs.getString("DIETNAME"),
+						rs.getString("DIETPIC"));
+				genreList.add(genreResult);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			genreList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			genreList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					genreList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return genreList;
+	}
+
+	public List<Recd> selectHarvest(int harvest) {
+		Connection conn = null;
+		List<Recd> genreList = new ArrayList<Recd>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/test", "sa", "");
+
+			// SQL文を準備する
+			String sql = "SELECT DIETNAME,DIETPIC FROM RECD WHERE HARVEST = ? ORDER BY RAND() LIMIT 3";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, harvest);
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Recd genreResult = new Recd(
+						rs.getString("DIETNAME"),
+						rs.getString("DIETPIC"));
+				genreList.add(genreResult);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			genreList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			genreList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					genreList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return genreList;
+	}
+}
